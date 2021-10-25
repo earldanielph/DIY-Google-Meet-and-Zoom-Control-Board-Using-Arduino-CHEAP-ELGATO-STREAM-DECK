bd9eed1bd0c5a60f49024b05a38232ccf80fe88d
Evtech <36310761+earldanielph@users.noreply.github.com>
2021-10-25 13:15:03 +0800

    Add files via upload

---
+++ Arduino Code/Arduino_Code_For_Google_Meet_Control_Board/Arduino_Code_For_Google_Meet_Control_Board.ino
@@ -0,0 +1,141 @@
+// Arduino Code For Google Meet Control Board
+// Evtech | @earldanielph
+
+#include "HID-Project.h"
+
+#define GOOGLEBTNS
+
+// Define Arduino pin numbers for buttons and LEDs
+#define led 13
+
+#ifdef GOOGLEBTNS
+#define BTN1 8
+#define BTN2 9
+#define BTN3 10
+#define BTN4 A0
+#define BTN5 A1
+#define BTN6 A2
+#define BTN7 A3
+#define NUMBUTTONS 7
+int buttons[NUMBUTTONS] = {BTN1, BTN2, BTN3, BTN4, BTN5, BTN6, BTN7};
+unsigned long btntime[NUMBUTTONS];
+boolean btnpress[NUMBUTTONS];
+#endif
+
+char line[80];
+unsigned long t;
+int n;
+
+void setup() {
+
+#ifdef GOOGLEBTNS
+  for (int i = 0; i < NUMBUTTONS; i++) {
+    pinMode(buttons[i], INPUT_PULLUP);
+    btntime[i] = 0;
+    btnpress[i] = false;
+  }
+#endif
+}
+
+void loop() {
+  int leds;
+#ifdef GOOGLEBTNS
+  for (int i = 0; i < NUMBUTTONS; i++) {
+    if (!digitalRead(buttons[i])) {
+      // Button pressed (negative logic)
+      if (btntime[i] == 0) {
+        // Button has just been pressed
+        btntime[i] = millis();
+      }
+      else {
+        // Button is still pressed
+        if (millis() - btntime[i] > 20 && !btnpress[i]) {
+          // This is not just a glitch          
+          btnpress[i] = true;
+          // Now do your stuff!
+          digitalWrite(led, HIGH);
+          switch (i) {
+#if 1
+          case 0: // Ctrl-Alt-k: Increase the number of participant tiles
+            BootKeyboard.press(KEY_LEFT_CTRL);
+            BootKeyboard.press(KEY_LEFT_ALT);
+            BootKeyboard.write(KEY_K);
+            BootKeyboard.release(KEY_LEFT_CTRL);
+            BootKeyboard.release(KEY_LEFT_ALT);
+            break;
+          case 1: // Ctrl-Alt-j: Decrease the number of participant tiles
+            BootKeyboard.press(KEY_LEFT_CTRL);
+            BootKeyboard.press(KEY_LEFT_ALT);
+            BootKeyboard.write(KEY_J);
+            BootKeyboard.release(KEY_LEFT_CTRL);
+            BootKeyboard.release(KEY_LEFT_ALT);            
+            break;
+          case 2: // Alt-M: Mute all
+            BootKeyboard.press(KEY_LEFT_ALT);
+            BootKeyboard.write(KEY_M);
+            BootKeyboard.release(KEY_LEFT_ALT);
+            break;
+          case 3: // Ctrl-D: Mute or unmute your microphone
+            BootKeyboard.press(KEY_LEFT_CTRL);
+            BootKeyboard.write(KEY_D);
+            BootKeyboard.release(KEY_LEFT_CTRL);
+            break;
+          case 4: //Ctrl-Alt-C: Show or hide meeting chat window
+            BootKeyboard.press(KEY_LEFT_CTRL);
+            BootKeyboard.press(KEY_LEFT_ALT);
+            BootKeyboard.write(KEY_C);
+            BootKeyboard.release(KEY_LEFT_CTRL);
+            BootKeyboard.release(KEY_LEFT_ALT);
+            break;
+          case 5: // Ctrl + Alt + h: Raise or lower your hand
+            BootKeyboard.press(KEY_LEFT_CTRL);
+            BootKeyboard.press(KEY_LEFT_ALT);
+            BootKeyboard.write(KEY_H);
+            BootKeyboard.release(KEY_LEFT_CTRL);
+            BootKeyboard.release(KEY_LEFT_ALT);
+            break;
+          case 6: // Ctrl-E: Turn on or off camera
+            BootKeyboard.press(KEY_LEFT_CTRL);
+            BootKeyboard.write(KEY_E);
+            BootKeyboard.release(KEY_LEFT_CTRL);
+            break;
+#else
+          case 0:
+            BootKeyboard.write(KEY_0);
+            break;
+          case 1:
+            BootKeyboard.write(KEY_1);
+            break;
+          case 2:
+            BootKeyboard.write(KEY_2);
+            break;
+          case 3:
+            BootKeyboard.write(KEY_3);
+            break;
+          case 4:
+            BootKeyboard.write(KEY_4);
+            break;
+          case 5:
+            BootKeyboard.write(KEY_5);
+            break;
+          case 6:
+            BootKeyboard.write(KEY_6);
+            break;
+#endif
+          }  
+        }
+      }
+    }
+    else {
+      // Button not pressed
+      if (btntime[i] != 0) {
+        // Looks like it's just been released
+        digitalWrite(led, LOW);
+        btntime[i] = 0;
+        btnpress[i] = false;
+      }
+    }
+  }
+#endif
+
+}
